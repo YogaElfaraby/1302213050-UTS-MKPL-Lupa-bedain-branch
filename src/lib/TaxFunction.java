4714da7ce9733e94ec1cceae6ac7d8ae8c627789
YogaElfaraby <100010908+YogaElfaraby@users.noreply.github.com>
2024-04-21 22:14:42 +0700

    Memperbaiki class TaxFunction
    Menambahkan

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,8 +2,8 @@ package lib;
 
 public class TaxFunction {
 
-	
-	/**
+
+		/**
 	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
 	 * 
 	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
@@ -14,37 +14,42 @@ public class TaxFunction {
 	 * 
 	 */
 	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
+	private static final double TAX_RATE = 0.05;
+    private static final double BASE_TAX_FREE_INCOME = 54000000;
+    private static final double ADDITIONAL_TAX_FREE_INCOME_PER_CHILD = 4500000;
+    private static final int MAX_CHILDREN_FOR_TAX_FREE_INCOME = 3;
+
+	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthsWorked, int deductible, boolean isMarried, int numberOfChildren) {
+        validateMonthsWorked(numberOfMonthsWorked);
+        
+        double totalIncome = calculateTotalIncome(monthlySalary, otherMonthlyIncome, numberOfMonthsWorked);
+        double totalTaxFreeIncome = calculateTotalTaxFreeIncome(isMarried, numberOfChildren);
+        double taxableIncome = totalIncome - deductible - totalTaxFreeIncome;
+
+        int tax = (int) Math.round(TAX_RATE * taxableIncome);
+        return Math.max(tax, 0);
+    }
 	
 	private static void validateMonthsWorked(int numberOfMonthsWorked) {
         if (numberOfMonthsWorked > 12) {
             throw new IllegalArgumentException("Number of months worked cannot exceed 12 per year.");
         }
     }
+
+	private static double calculateTotalIncome(int monthlySalary, int otherMonthlyIncome, int numberOfMonthsWorked) {
+        return (monthlySalary + otherMonthlyIncome) * numberOfMonthsWorked;
+    }
+
+	private static double calculateTotalTaxFreeIncome(boolean isMarried, int numberOfChildren) {
+        double totalTaxFreeIncome = BASE_TAX_FREE_INCOME;
+
+        if (isMarried) {
+            totalTaxFreeIncome += numberOfChildren * ADDITIONAL_TAX_FREE_INCOME_PER_CHILD;
+            totalTaxFreeIncome += Math.min(numberOfChildren, MAX_CHILDREN_FOR_TAX_FREE_INCOME - 1) * ADDITIONAL_TAX_FREE_INCOME_PER_CHILD;
+        }
+
+        return totalTaxFreeIncome;
+    }
+
 	
 }
